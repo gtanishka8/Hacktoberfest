a4efc9dd3ec93c5a7c4ce05805dee7d052a9a767
gtanishka8 <gtanishka8@gmail.com>
2019-10-25 18:06:03 +0530

    prandrome code edited

--- Python/palindrome.py
+++ Python/palindrome.py
@@ -1,23 +1,5 @@
-## test palindrome for input text
-
-# function which return reverse of a string 
-def reverse(s): 
-        return s[::-1] 
-
-def isPalindrome(s):
-    # Calling reverse function 
-    rev = reverse(s) 
-    # Checking if both string are equal or not 
-    if (s == rev): 
-        return True
-    return False
-
-# Driver code 
-s = str(input("Please enter the text: \n"))
-ans = isPalindrome(s.lower()) 
-
-if ans == 1: 
+n=input()
+if (n==n[::-1]): 
     print("Yes, It's a PALINDROME") 
-    
-else: 
-    `print("Not PALINDROME")
+else:
+    print("Not PALINDROME")
