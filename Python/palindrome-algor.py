09ab03645cb6fd0a69e09607a3763c601bdb5438
gtanishka8 <gtanishka8@gmail.com>
2019-10-25 18:07:27 +0530

    palindrome code edited

--- Python/palindrome-algor.py
+++ Python/palindrome-algor.py
@@ -1,22 +1,5 @@
-
-#This is my favorite python algorithm
-#because I am still very much a beginner
-#and this was the very first algorithm
-#I was able to figure out on my own without
-#any searching through StackOverflow
-
-def reverse(text):
-    """Takes a string and returns the reverse"""
-    
-    rev_word = ""
-    for char in text:
-        rev_word = char + rev_word
-        
-    return rev_word
-
-def is_palindrome(text):
-    """checks to see if string is a palindrome"""
-    
-    return text == reverse(text)
-    
-print(is_palindrome('racecar'))
+n=input()
+if (n==n[::-1]): 
+    print("Yes, It's a PALINDROME") 
+else:
+    print("Not PALINDROME")
